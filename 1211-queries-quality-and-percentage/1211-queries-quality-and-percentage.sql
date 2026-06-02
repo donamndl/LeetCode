867a64d3320214ee3910685d344cd00463cf0b3e
Dona Mondal <180500141+donamndl@users.noreply.github.com>
2026-06-02 21:35:45 +0530

    Time: 384 ms (36.03%), Space: 0B (100%) - LeetHub

---
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+SELECT QUERY_NAME ,
+ROUND(SUM(RATING/POSITION) / COUNT(*),2) AS QUALITY,
+ROUND(SUM(
+    CASE WHEN RATING < 3 THEN 1 ELSE 0 END) 
+    / COUNT(*) * 100,2) AS POOR_QUERY_PERCENTAGE
+FROM QUERIES
+GROUP BY QUERY_NAME;
